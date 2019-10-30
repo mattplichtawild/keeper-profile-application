e0d9fd3dfe7618852298c9c961362e528bfed0ae
mplichta <bigcatplichta@gmail.com>
2019-10-30 14:54:36 -0600

    edited controller routes

--- learn-projects/keeper-profile/app/controllers/keeper_controller.rb
+++ learn-projects/keeper-profile/app/controllers/keeper_controller.rb
@@ -2,45 +2,49 @@ class ZookeeperController < ApplicationController
 
     get '/login' do
         #sets user id as session user_id
+        erb :'/keepers/login'
     end
 
     get '/logout' do
         #clears session hash
+        erb :'/keepers/logout'
     end
 
-    get '/user/new' do
+    get '/account/new' do
         #create new user
+        erb :'/keepers/new'
     end
 
-    post '/user/new' do
+    post '/account/new' do
         #send form info to zookeeper model to create new user
+        redirect to "/account/#{user.id}"
     end
 
-    get '/user/:id' do
+    get '/account/:id' do
         #display user account/profile page
     end
 
-    get '/user/:id/animals' do
+    get '/account/:id/animals' do
         #display index of animals owned by user
     end
 
-    get '/user/:id/animals/new' do
+    get '/account/:id/animals/new' do
         #display form to create new animal
     end
 
-    post '/user/:id/animals/new' do
+    post '/account/:id/animals/new' do
         #sends info from form to animal model to create new animal
     end
 
-    get '/user/:id/animals/:animal_id' do
+    get '/account/:id/animals/:animal_id' do
         #display specific animal profile page
     end
 
-    get '/user/:id/animals/:animal_id/edit' do
+    get '/account/:id/animals/:animal_id/edit' do
         #edit the specific animal
     end
 
-    get '/user/:id/edit' do
+    get '/account/:id/edit' do
         #display edit form for user
     end
 
