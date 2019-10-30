8a61167197fc4b3ac1e41da8adb1a09b8cbaa038
mplichta <bigcatplichta@gmail.com>
2019-10-30 14:28:25 -0600

    added controller methods to keeper

--- learn-projects/keeper-profile/app/controllers/keeper_controller.rb
+++ learn-projects/keeper-profile/app/controllers/keeper_controller.rb
@@ -1,20 +1,49 @@
 class ZookeeperController < ApplicationController
 
     get '/login' do
-
+        #sets user id as session user_id
     end
 
     get '/logout' do
-
+        #clears session hash
     end
 
-    get '/account/new' do
-    
+    get '/user/new' do
+        #create new user
     end
 
-    get '/account/:id' do
-        
+    post '/user/new' do
+        #send form info to zookeeper model to create new user
     end
 
-    
+    get '/user/:id' do
+        #display user account/profile page
+    end
+
+    get '/user/:id/animals' do
+        #display index of animals owned by user
+    end
+
+    get '/user/:id/animals/new' do
+        #display form to create new animal
+    end
+
+    post '/user/:id/animals/new' do
+        #sends info from form to animal model to create new animal
+    end
+
+    get '/user/:id/animals/:animal_id' do
+        #display specific animal profile page
+    end
+
+    get '/user/:id/animals/:animal_id/edit' do
+        #edit the specific animal
+    end
+
+    get '/user/:id/edit' do
+        #display edit form for user
+    end
+
+
+
 end
