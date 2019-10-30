20b5800e76e00e2c9d73227eba564731a963d62d
mplichta <bigcatplichta@gmail.com>
2019-10-30 15:04:58 -0600

    added to controller routes

--- learn-projects/keeper-profile/app/controllers/keeper_controller.rb
+++ learn-projects/keeper-profile/app/controllers/keeper_controller.rb
@@ -22,32 +22,37 @@ class ZookeeperController < ApplicationController
 
     get '/account/:id' do
         #display user account/profile page
+        erb :'/keepers/show'
     end
 
     get '/account/:id/animals' do
         #display index of animals owned by user
+        erb :'/animals/index'
     end
 
     get '/account/:id/animals/new' do
         #display form to create new animal
+        erb :'/animals/new'
     end
 
     post '/account/:id/animals/new' do
         #sends info from form to animal model to create new animal
+        redirect to "/account/#{user.id}/animals/#{animal.id}"
     end
 
     get '/account/:id/animals/:animal_id' do
         #display specific animal profile page
+        erb :'/animals/show'
     end
 
     get '/account/:id/animals/:animal_id/edit' do
         #edit the specific animal
+        erb :'/animals/edit'
     end
 
     get '/account/:id/edit' do
         #display edit form for user
+        erb :'/keepers/edit'
     end
-
-
 
 end
