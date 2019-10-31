41f8a130703832cec590d863da943cff54e196fb
mplichta <bigcatplichta@gmail.com>
2019-10-30 22:45:53 -0600

    migration to add foreign key to animals table

--- learn-projects/keeper-profile/db/schema.rb
+++ learn-projects/keeper-profile/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191030172641) do
+ActiveRecord::Schema.define(version: 20191031043504) do
 
   create_table "animals", force: :cascade do |t|
     t.string "name"
